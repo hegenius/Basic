346041cd90a3542e8423855285e0d57e6a5ed2ae
hegenius <hegenius1990@gmail.com>
2024-06-04 20:45:18 +0900

    study01 -  변수, 데이터 타입, 타입변환

---
+++ java/study01/src/Main.java
@@ -0,0 +1,13 @@
+//Main 클래스 명이면서 파일명
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+        System.out.println("자바 프로그램을 만들어 보았습니다.");
+    }
+}
+
