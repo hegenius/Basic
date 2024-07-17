5327c521364298caed59b188a7363e4c2e79b17d
hegenius <hegenius1990@gmail.com>
2024-07-17 17:44:30 +0900

    Spring study07 - json 데이터 파싱

---
+++ Spring/xml_json_parser/src/main/java/bitc/fullstack405/xml_json_parser/dto/kobis/BoxOfficeResultDTO.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.xml_json_parser.dto.kobis;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BoxOfficeResultDTO {
+  private String boxofficeType;
+  private String showRange;
+  private List<BoxOfficeResultDTO> dailyBoxOfficeList;
+
+}
